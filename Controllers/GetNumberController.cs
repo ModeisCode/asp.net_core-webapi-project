98a6ec9d5e44bac4ed51248f160353e8bd6bb965
SoftwareArchitecture <100340196+ModeisCode@users.noreply.github.com>
2024-07-20 07:24:48 +0300

    Add files via upload

---
+++ Controllers/GetNumberController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using WebApplication1.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetNumberController : ControllerBase
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public GetNumberController(IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase("users");
+            _users = database.GetCollection<User>("usersInfo");
+        }
+
+
+        [HttpGet]
+        public async Task<List<User>> Get()
+        {
+            return await _users.Find(_ => true).ToListAsync();
+        }
+
+        // GET api/<GetNumberController>/5
+        [HttpGet("{age}")]
+        public async Task<ActionResult<User>> Get(int age)
+        {
+            var filter = Builders<User>.Filter.Eq(user => user.age, age);
+            var user = await _users.Find(filter).FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // POST api/<GetNumberController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GetNumberController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GetNumberController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public int? age { get; set; }
+        public string? city { get; set; }
+    }
+}
